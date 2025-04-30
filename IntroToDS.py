a87f3053f1877d6a073fd22c426df642245e05a1
PythonIn_the808 <70725532+sunnyz80@users.noreply.github.com>
2025-04-29 15:30:30 -1000

    Update IntroToDS.py

--- IntroToDS.py
+++ IntroToDS.py
@@ -11,12 +11,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-file_path = '1000SalesRecords.csv'  # Update this path if the file is located in a different directory
+#read file
+file_path = '1000SalesRecords.csv'  
 df = pd.read_csv(file_path)
 
 # Preparing the data
-X = df[['Year']]
-y = df['Sales']
+X = df[['Order Date']]
+y = df['Total Revenue']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Training the model
